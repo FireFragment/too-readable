3932d83f0b4998d72a669043cce434dedb0aee61
FireFragment <55660550+FireFragment@users.noreply.github.com>
2021-09-24 13:50:40 +0200

    Remove extra linebreaks on `unparsed.cpp`

--- src/parse-states/unparsed.cpp
+++ src/parse-states/unparsed.cpp
@@ -1,10 +1,6 @@
 #include "unparsed.h"
 
-
-
 std::string TooReadable::ParseStates::Unparsed::ContinueWith(std::string continueWith) {
-    
-    
     // This string should be equal to continueWith.
     std::string importantPortion = TheCode.substr(currentPosForContinueWith, continueWith.length());
 
