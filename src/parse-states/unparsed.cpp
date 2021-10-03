04056ecdd5a5a9fdc4d4bde3b0165f12873e069a
FireFragment <55660550+FireFragment@users.noreply.github.com>
2021-10-03 17:10:44 +0200

    Add Unparsed.skipTo

--- src/parse-states/unparsed.cpp
+++ src/parse-states/unparsed.cpp
@@ -13,3 +13,18 @@ std::string TooReadable::ParseStates::Unparsed::ContinueWith(std::string continu
 
     return returnVal;
 };
+
+std::string TooReadable::ParseStates::Unparsed::SkipTo(std::string skipTo) {
+    
+    size_t occurencePos = TheCode.find(skipTo, currentPosForContinueWith);
+    if (occurencePos == std::string::npos) // `skipTo` not found in `TheCode`
+        throw SkipToArgNotFoundException(skipTo);
+    
+    // Compute return value.
+    std::string out = TheCode.substr(currentPosForContinueWith, occurencePos - currentPosForContinueWith);
+    
+    // Update `currentPos` for next run
+    currentPosForContinueWith = occurencePos + skipTo.length();
+    
+    return out;
+}
--- src/parse-states/unparsed.h
+++ src/parse-states/unparsed.h
@@ -2,13 +2,14 @@
 #define TOOREADABLE_PARSESTATES_UNPARSED_H
 
 #include <string>
+#include "../exception.h"
 
 namespace TooReadable::ParseStates
 {
 
 /** \class Unparsed unparsed.h "parse-states/unparsed.h"
  *  \brief This class represents non-parsed TOR program.
- * 
+ *
  *  Adds some useful functions
  */
 class Unparsed
@@ -16,13 +17,32 @@ class Unparsed
 public:
     std::string TheCode;
     Unparsed(std::string TheCode):TheCode(TheCode) {};
-    
+
     // TODO: Write better docs
     /**
      * \param[in] continueWith String, that the program should continue with
      */
     std::string ContinueWith(std::string continueWith);
-    
+
+    /**
+     * \param[in] skipTo String, to that we should skip.
+     * \returns String, we just skipped.
+     */
+    std::string SkipTo(std::string skipTo);
+
+    /**
+     * \exception SkipToArgNotFoundException
+     */
+    class SkipToArgNotFoundException:public Exception {
+    public:
+        SkipToArgNotFoundException(std::string whatNotFound):whatNotFound(whatNotFound) {}
+        std::string whatNotFound;
+        const std::string what()
+        {
+            return whatNotFound + " not found in the code!";
+        };
+    };
+
 private:
     /**
      * Current position for \c ContinueWith function.
