220d5d20397fd392b7ea49b7c853283050a44e58
FireFragment <55660550+FireFragment@users.noreply.github.com>
2021-10-04 20:28:22 +0200

    Update tests to not fail

--- tests.cpp
+++ tests.cpp
@@ -8,7 +8,7 @@
 TEST ( unparsed_continueWith, true_case )
 {
     TooReadable::ParseStates::Unparsed test = std::string("abcdef");
-    EXPECT_EQ(test.ContinueWith("abc"), "");
+    EXPECT_EQ(test.ContinueWith("abc", false), "");
 }
 
 /**
@@ -17,7 +17,7 @@ TEST ( unparsed_continueWith, true_case )
 TEST ( unparsed_continueWith, false_case )
 {
     TooReadable::ParseStates::Unparsed test = std::string("abcdef");
-    EXPECT_EQ(test.ContinueWith("test"), "abcd");
+    EXPECT_EQ(test.ContinueWith("test", false), "abcd");
 }
 
 /**
@@ -26,9 +26,9 @@ TEST ( unparsed_continueWith, false_case )
 TEST ( unparsed_continueWith, multiple_cases )
 {
     TooReadable::ParseStates::Unparsed test = std::string("abcdefghijk");
-    EXPECT_EQ(test.ContinueWith("abc"), "");
-    EXPECT_EQ(test.ContinueWith("def"), "");
-    EXPECT_EQ(test.ContinueWith("test"), "ghij");
+    EXPECT_EQ(test.ContinueWith("abc", false), "");
+    EXPECT_EQ(test.ContinueWith("def", false), "");
+    EXPECT_EQ(test.ContinueWith("test", false), "ghij");
 }
 
 /**
@@ -37,14 +37,14 @@ TEST ( unparsed_continueWith, multiple_cases )
 TEST ( unparsed_continueWith, multiple_cases_and_instances )
 {
     TooReadable::ParseStates::Unparsed test1 = std::string("abcdefghijklmno");
-    EXPECT_EQ(test1.ContinueWith("abc"), "");
-    EXPECT_EQ(test1.ContinueWith("def"), "");
+    EXPECT_EQ(test1.ContinueWith("abc", false), "");
+    EXPECT_EQ(test1.ContinueWith("def", false), "");
     TooReadable::ParseStates::Unparsed test2 = std::string("testing_str");
-    EXPECT_EQ(test1.ContinueWith("ghi"), "");
-    EXPECT_EQ(test2.ContinueWith("test"), "");
-    EXPECT_EQ(test2.ContinueWith("Hello!"), "ing_st");
-    EXPECT_EQ(test1.ContinueWith("jk"), "");
-    EXPECT_EQ(test1.ContinueWith("end"), "lmn");
+    EXPECT_EQ(test1.ContinueWith("ghi", false), "");
+    EXPECT_EQ(test2.ContinueWith("test", false), "");
+    EXPECT_EQ(test2.ContinueWith("Hello!", false), "ing_st");
+    EXPECT_EQ(test1.ContinueWith("jk", false), "");
+    EXPECT_EQ(test1.ContinueWith("end", false), "lmn");
 }
 
 /**
@@ -53,10 +53,10 @@ TEST ( unparsed_continueWith, multiple_cases_and_instances )
 TEST ( unparsed_continueWith, true_case_after_false_case )
 {
     TooReadable::ParseStates::Unparsed test = std::string("abcdefghijk");
-    EXPECT_EQ(test.ContinueWith("abc"), "");
-    EXPECT_EQ(test.ContinueWith("def"), "");
-    EXPECT_EQ(test.ContinueWith("test"), "ghij");
-    EXPECT_EQ(test.ContinueWith("gh"), "");
+    EXPECT_EQ(test.ContinueWith("abc", false), "");
+    EXPECT_EQ(test.ContinueWith("def", false), "");
+    EXPECT_EQ(test.ContinueWith("test", false), "ghij");
+    EXPECT_EQ(test.ContinueWith("gh", false), "");
 }
 
 /**
@@ -65,7 +65,7 @@ TEST ( unparsed_continueWith, true_case_after_false_case )
 TEST ( unparsed_continueWith, overhang )
 {
     TooReadable::ParseStates::Unparsed test1 = std::string("abcd");
-    EXPECT_EQ(test1.ContinueWith("super_test"), "abcd");   
+    EXPECT_EQ(test1.ContinueWith("super_test", false), "abcd");   
 }
 
 /**
@@ -74,7 +74,7 @@ TEST ( unparsed_continueWith, overhang )
 TEST ( unparsed_continueWith, overhang_almost_true )
 {
     TooReadable::ParseStates::Unparsed test1 = std::string("abcd");
-    EXPECT_EQ(test1.ContinueWith("abcdef"), "abcd");   
+    EXPECT_EQ(test1.ContinueWith("abcdef", false), "abcd");   
 }
 
 /**
@@ -88,7 +88,7 @@ TEST ( unparsed_skipTo, basic )
     EXPECT_EQ(test1.SkipTo("nopq"), "jklm");  
     EXPECT_THROW({
         test1.SkipTo("opq");
-    }, TooReadable::ParseStates::Unparsed::SkipToArgNotFoundException);   
+    }, TooReadable::ParseStates::Unparsed::ArgNotFoundException);   
     
     EXPECT_EQ(test1.SkipTo("uv"), "rst");  
 }
@@ -104,7 +104,7 @@ TEST ( unparsed, skipTo_continueWith )
     EXPECT_EQ(test1.ContinueWith("jkl"), "");   
     EXPECT_THROW({
         test1.SkipTo("lmn");
-    }, TooReadable::ParseStates::Unparsed::SkipToArgNotFoundException);   
+    }, TooReadable::ParseStates::Unparsed::ArgNotFoundException);   
     
     EXPECT_EQ(test1.SkipTo("uv"), "mnopqrst");  
 }
