8520c55bd8171eacf5eb447f6f771653af1abfed
FireFragment <55660550+FireFragment@users.noreply.github.com>
2021-10-11 20:46:33 +0200

    Parse correctly programs with multiple functions.

--- src/parse-states/divided.cpp
+++ src/parse-states/divided.cpp
@@ -5,28 +5,35 @@ TooReadable::ParseStates::Divided::Divided(Unparsed original)
     // If it isn't a library, the "please" definition of the main function.
     if (original.ContinueWith("Please ", false) == "")
         mainFunc = original.SkipTo(".\n");
-    original.ContinueWith("\n");
     
-    // Function header
-    original.ContinueWith("How to ");
-    Function func;
-    func.name = original.SkipTo("\n");
-    
-    // TODO: Replace `7` with something more clear (it's length of "How to ")
-    original.ContinueWith(std::string(func.name.length() + 7, '=') + "\n\n");
-    
-    // Function steps
-    int i = 1;
-    
-    // Catch thrown exception, when last step of the function reached.
+    // Functions
     try {
         while (true) {
-            original.ContinueWith(" " + std::to_string(i) + ". "); // May throw exception
-            func.steps.push_back(original.SkipTo(".\n"));
+            original.ContinueWith("\n");
             
-            i++;
+            // Function header
+            original.ContinueWith("How to ");
+            Function func;
+            func.name = original.SkipTo("\n");
+            
+            // TODO: Replace `7` with something more clear (it's length of "How to ")
+            original.ContinueWith(std::string(func.name.length() + 7, '=') + "\n\n");
+            
+            // Function steps
+            int i = 1;
+            
+            // Catch thrown exception, when last step of the function reached.
+            try {
+                while (true) {
+                    original.ContinueWith(" " + std::to_string(i) + ". "); // May throw exception
+                    func.steps.push_back(original.SkipTo(".\n"));
+                    
+                    i++;
+                }
+            } catch (Unparsed::ArgNotFoundException err) {}
+            
+            functions.push_back(func);
         }
     } catch (Unparsed::ArgNotFoundException err) {}
     
-    functions.push_back(func);
 }
