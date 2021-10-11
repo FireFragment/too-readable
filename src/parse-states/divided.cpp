1a06ba87532aee655db6f348c92cb4789d210d39
FireFragment <55660550+FireFragment@users.noreply.github.com>
2021-10-11 20:21:36 +0200

    Parse the first function in `Divided.Divided`

--- src/parse-states/divided.cpp
+++ src/parse-states/divided.cpp
@@ -6,4 +6,27 @@ TooReadable::ParseStates::Divided::Divided(Unparsed original)
     if (original.ContinueWith("Please ", false) == "")
         mainFunc = original.SkipTo(".\n");
     original.ContinueWith("\n");
+    
+    // Function header
+    original.ContinueWith("How to ");
+    Function func;
+    func.name = original.SkipTo("\n");
+    
+    // TODO: Replace `7` with something more clear (it's length of "How to ")
+    original.ContinueWith(std::string(func.name.length() + 7, '=') + "\n\n");
+    
+    // Function steps
+    int i = 1;
+    
+    // Catch thrown exception, when last step of the function reached.
+    try {
+        while (true) {
+            original.ContinueWith(" " + std::to_string(i) + ". "); // May throw exception
+            func.steps.push_back(original.SkipTo(".\n"));
+            
+            i++;
+        }
+    } catch (Unparsed::ArgNotFoundException err) {}
+    
+    functions.push_back(func);
 }
