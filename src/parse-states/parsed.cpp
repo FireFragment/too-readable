b1f537cf7fd653c2fca347105fe3263f98e262ff
FireFragment <55660550+FireFragment@users.noreply.github.com>
2021-11-15 19:31:03 +0100

    Create `Parsed` parse state

---
+++ src/parse-states/parsed.cpp
@@ -0,0 +1,6 @@
+#include "parsed.h"
+
+TooReadable::ParseStates::Parsed::Parsed()
+{
+
+}
---
+++ src/parse-states/parsed.h
@@ -0,0 +1,21 @@
+#ifndef TOOREADABLE_PARSESTATES_PARSED_H
+#define TOOREADABLE_PARSESTATES_PARSED_H
+
+namespace TooReadable::ParseStates {
+
+/**
+ * @todo write docs
+ */
+class Parsed
+{
+public:
+    /**
+     * Default constructor
+     */
+    Parsed();
+
+};
+
+}
+
+#endif // TOOREADABLE_PARSESTATES_PARSED_H
