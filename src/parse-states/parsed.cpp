259283a702e18333ef617c9b6efe11537b2c990e
FireFragment <55660550+FireFragment@users.noreply.github.com>
2021-11-17 11:32:57 +0100

    Implement `Parsed`'s constructor

--- src/parse-states/parsed.cpp
+++ src/parse-states/parsed.cpp
@@ -1,5 +1,42 @@
 #include "parsed.h"
 
 TooReadable::ParseStates::Parsed::Parsed(Divided original)
-{
+{    
+    // Copy name of original's functions to `funcs`.
+    // Steps will be parsed soon.
+    for (Divided::Function func : original.functions) { 
+        funcs.push_back(Function(func.name));
+        
+        if (original.mainFunc == func.name)
+            mainFunc = &(funcs.back());
+    }
+    
+    // Parse steps
+    // Iterate through `original.functions` and `funcs` at once.
+    std::vector<Function>::iterator thisFunc = funcs.begin();
+    for (Divided::Function func : original.functions) {
+        std::vector<Step> steps; // Steps of `func`
+        
+        // Parse each step
+        for (std::string stepStr : func.steps) {
+            
+            Function* stepFunc = 0x000000;
+            
+            // Find function named `step` and save it to `stepFunc`
+            for (Function& i : funcs)
+                if (i.name == stepStr) {
+                    stepFunc = &i;
+                    break;
+                }
+            
+            if (stepFunc == 0x000000) { // No function named `step` found
+                throw FuncNotDefined(stepStr);
+            }
+            
+            steps.push_back(Step(stepFunc));
+        }
+        
+        thisFunc->body = steps;
+        thisFunc++;
+    }
 }
