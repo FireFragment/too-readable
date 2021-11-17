caadc0fe81996b6d1c72ccab5774df091b83cafb
FireFragment <55660550+FireFragment@users.noreply.github.com>
2021-11-17 19:10:03 +0100

    Add builtin functions

---
+++ src/builtinfunction.cpp
@@ -0,0 +1,19 @@
+#include "builtinfunction.h"
+
+using namespace TooReadable;
+
+void TooReadable::BuiltinFunction::run() {
+    toRun();
+}
+
+void TooReadable::BuiltinFuncs::HelloWorld() {
+    std::cout << "Hello world" << std::endl;
+}
+
+void TooReadable::BuiltinFuncs::Greet() {
+    std::cout << "Hello my friend" << std::endl;
+}
+
+void TooReadable::BuiltinFuncs::Advert() {
+    std::cout << "TooReadable is the best!" << std::endl;
+}
---
+++ src/builtinfunction.h
@@ -0,0 +1,52 @@
+#ifndef TOOREADABLE_BUILTINFUNCTION_H
+#define TOOREADABLE_BUILTINFUNCTION_H
+
+#include <iostream>
+#include <vector>
+#include "parse-states/parsed.h"
+
+namespace TooReadable
+{
+
+/**
+ * \brief Builtin TooReadable function
+ */
+class BuiltinFunction : public ParseStates::Parsed::Function
+{
+public:
+    
+    BuiltinFunction() {};
+    BuiltinFunction(std::string name_, void (*toRun)()): toRun(toRun) {name = name_;};
+    
+    /**
+     * \brief What should be ran when calling this function
+     */
+    void (*toRun)();
+    
+    /**
+     * \brief Run the function
+     */
+    void run();
+};
+
+
+namespace BuiltinFuncs
+{
+    void HelloWorld();
+    void Greet();
+    void Advert();
+    
+    /**
+     * \brief List of all TooReadable builtin functions
+     */
+    const std::vector<BuiltinFunction> list = {
+        BuiltinFunction("Greet the world",        &HelloWorld), // Prints `Hello world` to the standart output.
+        BuiltinFunction("Greet the user",         &Greet),      // Prints `Hello my friend` to the standart output.
+        BuiltinFunction("Propagate TooReadable",  &Advert)      // Prints `TooReadable is the best!` to the standart output.
+    };
+    
+}
+
+}
+
+#endif // TOOREADABLE_BUILTINFUNCTION_H
