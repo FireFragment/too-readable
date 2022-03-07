80266a88fbd48de6a83722e383571b35c6383fbb
FireFragment <maty78485@gmail.com>
2022-03-07 12:59:24 +0100

    Make builtin functions return `Value` + add `GetInput` builtin function

--- src/builtinfunction.cpp
+++ src/builtinfunction.cpp
@@ -4,30 +4,41 @@ using namespace TooReadable;
 
 const TooReadable::Value TooReadable::BuiltinFunction::run(std::vector< TooReadable::Value > args)
 {
-    toRun(args);
+    return toRun(args);
+}
+
+Value TooReadable::BuiltinFuncs::HelloWorld(std::vector<Value> args) {
+    std::cout << "Hello world" << std::endl;
     return Value();
 }
 
-void TooReadable::BuiltinFuncs::HelloWorld(std::vector<Value> args) {
-    std::cout << "Hello world" << std::endl;
-}
-
-void TooReadable::BuiltinFuncs::Greet(std::vector<Value> args) {
+Value TooReadable::BuiltinFuncs::Greet(std::vector<Value> args) {
     std::cout << "Hello my friend" << std::endl;
+    return Value();
 }
 
-void TooReadable::BuiltinFuncs::Advert(std::vector<Value> args) {
+Value TooReadable::BuiltinFuncs::Advert(std::vector<Value> args) {
     std::cout << "TooReadable is the best!" << std::endl;
+    return Value();
 }
 
-void TooReadable::BuiltinFuncs::Echo (std::vector<Value> args)
+Value TooReadable::BuiltinFuncs::Echo (std::vector<Value> args)
 {
     std::cout << (std::string)args[0] << std::endl;
+    return Value();
 }
 
-void TooReadable::BuiltinFuncs::Test ( std::vector<Value> args )
+Value TooReadable::BuiltinFuncs::Test ( std::vector<Value> args )
 {
     std::cout << "Testing function launched.\nFIRST ARGUMENT:" << (std::string)args[0]
         << "\nSECOND ARGUMENT: " << (std::string)args[1]
         << "\nTHIRD ARGUMENT: "  << (std::string)args[2];
+    return Value();
+}
+
+TooReadable::Value TooReadable::BuiltinFuncs::GetInput(std::vector<Value> args)
+{
+    std::string str;
+    std::getline(std::cin, str);
+    return str;
 }
--- src/builtinfunction.h
+++ src/builtinfunction.h
@@ -16,13 +16,13 @@ class BuiltinFunction : public ParseStates::Parsed::Function
 public:
     
     BuiltinFunction() {};
-    BuiltinFunction(std::string name_, void (*toRun)(std::vector<Value> args), std::vector<std::string> _outOfLineArgs = {})
+    BuiltinFunction(std::string name_, Value (*toRun)(std::vector<Value> args), std::vector<std::string> _outOfLineArgs = {})
         :toRun(toRun) {outOfLineArgs = _outOfLineArgs; name = name_;};
     
     /**
      * \brief What should be ran when calling this function
      */
-    void (*toRun)(std::vector<Value> args);
+    Value (*toRun)(std::vector<Value> args);
     
     /**
      * \brief Run the function
@@ -33,21 +33,23 @@ public:
 
 namespace BuiltinFuncs
 {
-    void HelloWorld(std::vector<Value> args);
-    void Greet(std::vector<Value> args);
-    void Advert(std::vector<Value> args);
-    void Echo(std::vector<Value> args);
-    void Test(std::vector<Value> args);
-    
+    Value HelloWorld(std::vector<Value> args);
+    Value Greet(std::vector<Value> args);
+    Value Advert(std::vector<Value> args);
+    Value Echo(std::vector<Value> args);
+    Value Test(std::vector<Value> args);
+    Value GetInput(std::vector<Value> args);
+
     /**
      * \brief List of all TooReadable builtin functions
      */
     const std::vector<BuiltinFunction*> list = {
-        new BuiltinFunction("Greet the world",        &HelloWorld), // Prints `Hello world` to the standart output.
-        new BuiltinFunction("Greet the user",         &Greet),      // Prints `Hello my friend` to the standart output.
-        new BuiltinFunction("Propagate TooReadable",  &Advert),      // Prints `TooReadable is the best!` to the standart output.
-        new BuiltinFunction("Say something to user",  &Echo,      {"The message to say"}),
-        new BuiltinFunction("My testing function",    &Test,      {"First testing argument", "Second testing argument", "Third testing argument"})
+        new BuiltinFunction("Greet the world",          &HelloWorld), // Prints `Hello world` to the standart output.
+        new BuiltinFunction("Greet the user",           &Greet),      // Prints `Hello my friend` to the standart output.
+        new BuiltinFunction("Propagate TooReadable",    &Advert),      // Prints `TooReadable is the best!` to the standart output.
+        new BuiltinFunction("Say something to user",    &Echo,      {"The message to say"}),
+        new BuiltinFunction("My testing function",      &Test,      {"First testing argument", "Second testing argument", "Third testing argument"}),
+        new BuiltinFunction("Let user write something", &GetInput)
     };
     
 }
